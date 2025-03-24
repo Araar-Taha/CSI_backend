965dba3a2956cfbe64c2a184536f4809ad8c6a6b
Araar-Taha <araartaha@gmail.com>
2025-03-24 03:16:32 +0100

    Added view for user authentication,Not tested

---
+++ csi/authy/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Utilisateur(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('Participant', 'Participant'),
+        ('Woofer', 'Woofer'),
+        ('Admin', 'Admin'),
+    ]
+    #an AbstractUser django class have already the fields that we need
+    #we just need to add the user-type attribute
+    woofer = models.OneToOneField("Woofer", null=True, blank=True, on_delete=models.CASCADE)
+    participant = models.OneToOneField("Participant", null=True, blank=True, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=12, choices=USER_TYPE_CHOICES, default='User',)
+    #username is already included in REQUIRED_FIELDS so i removed it
+    REQUIRED_FIELDS = ['password','user_type']
+    def __str__(self):
+        return "{self.username} ({self.user_type})"
+    
+
+class Participant(models.Model):
+    user = models.OneToOneField(Utilisateur, on_delete=models.CASCADE, related_name='participant_instance')
+    # Add additional fields specific to Participant
+    # to add later:atelier FK
+
+    def __str__(self):
+        return self.user.username
+    
+
+
+class Woofer(models.Model):
+    user = models.OneToOneField(Utilisateur, on_delete=models.CASCADE, related_name='woofer_instance')
+    # Add additional fields specific to Woofer
+    participant_field = models.CharField(max_length=255)
+    dateDeNaissance = models.DateField()
+    dateDebutSejour = models.DateField()
+
+    def __str__(self):
+        return self.user.username
---
+++ csi/authy/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .serializers import UserSerializer
+from .models import Utilisateur,Participant,Woofer
+from rest_framework.viewsets import ViewSet
+from rest_framework.views import APIView
+from rest_framework.decorators import action
+from django.conf import settings
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+# TOKEN_EXPIRATION_ACCESS = 600
+# TOKEN_EXPIRATION_REFRESH = 1440
+
+# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]  # You can update this to any other permission later
+
+    def post(self, request):
+        user_type = request.data.get('user_type')
+        
+        
+        if user_type not in ['Participant', 'Woofer']:
+            return Response({'error': 'Invalid user type. Only "Participant" or "Woofer" allowed.'}, 
+                            status=status.HTTP_403_FORBIDDEN)
+
+        # Serialize the user data
+        serializer = UserSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()  # Save the user instance
+            
+            # Create the corresponding class instance based on user_type
+            if user_type == 'Participant':
+                Participant.objects.create(user=user)
+            elif user_type == 'Woofer':
+                Woofer.objects.create(user=user)
+
+    
+class LoginView(APIView):
+    permission_classes = [AllowAny]  # Allow access to anyone
+
+    def post(self, request, *args, **kwargs):
+        print(request.user)
+
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Check if username and password are provided
+        if not username or not password:
+            raise AuthenticationFailed('Username and password are required.')
+
+        user = Utilisateur.objects.filter(username=username).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
+        refresh_token = str(refresh)
+        access_token = str(access)
+        response = Response()
+
+        # Set the RefreshToken cookie
+        response.set_cookie(key='RefreshToken', value=refresh_token, httponly=True)
+
+        # Return access token and user type in the response
+        response.data = {
+            'AccessToken': access_token,
+            'user-type': user.user_type
+        }
+
+        return response 
+ 
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can log out
+
+    def post(self, request, *args, **kwargs):
+        response = Response()
+
+        # Delete the RefreshToken cookie
+        response.delete_cookie('RefreshToken')
+
+        # Return a success message
+        response.data = {
+            'message': 'success'
+        }
+
+        return response       
